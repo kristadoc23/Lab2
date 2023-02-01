9e5e4ffda4201bed3f65adae27b52bac92c9a1e1
kristadoc23 <123026546+kristadoc23@users.noreply.github.com>
2023-02-01 16:06:01 -0500

    Add files via upload

---
+++ L3.py
@@ -0,0 +1,8 @@
+#Krista Dockery
+import math
+a = math.tan(math.pi/4)+1
+b=math.sqrt(a)
+c=math.tan(math.pi/3)+math.sin(math.pi/3)
+den = math.sqrt(c)
+value = b/den
+print(value)
---
+++ L4.py
@@ -0,0 +1,8 @@
+#Krista Dockery
+import math
+r=4
+circle = math.pi*r**2
+a = 4*math.sqrt(2)
+sqr = a * a
+value = circle - sqr
+print(value)
